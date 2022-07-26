d440010d50534d54a2807c21a07a4b0c0e707aea
Ameer Mushani <amushani@uoguelph.ca>
2022-07-26 15:35:41 -0400

    Add tsparticles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import TopNav from './components/Navbar/Navbar';
+import About from './components/About/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PartticlesContainer from './components/ParticlesContainer';
 
 function App() {
+
   return (
     <div className="App">
+      <PartticlesContainer> </PartticlesContainer>
       <TopNav></TopNav>
+      <About></About>
+      
     </div>
   );
 }
