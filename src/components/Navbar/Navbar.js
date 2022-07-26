e313a60ff140277af4f50c8da3cd7ffe53113da6
Ameer Mushani <amushani@uoguelph.ca>
2022-07-26 14:14:46 -0400

    Right justified navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,18 @@
-import  Navbar  from "react-bootstrap/Navbar";
-import  Nav  from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 const TopNav = () => {
     return (
+        //bootstrap nav with right justified content
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Ameer Mushani</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="justify-content-end">
-                    <Nav.Link href="#home">About</Nav.Link>
-                    <Nav.Link href="#link">Temp</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+                <Navbar.Brand href="#home">
+                    Ameer Mushani
+                    {/* <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/a982ad29-1b19-4d8d-8a40-7285b3ed4465-profile_image-300x300.png" alt="logo" /> */}
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
         </Navbar>
     );
 }
