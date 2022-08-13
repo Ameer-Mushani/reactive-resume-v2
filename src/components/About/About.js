03d90c9a9e70c9a60a76949f3c390955f72e530c
Ameer Mushani <ameermushani@gmail.com>
2022-08-13 17:09:38 -0400

    Updated profile pic and fixed css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,48 +3,49 @@ import Typewriter from "typewriter-effect";
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import './About.css'
+import profilepic from "../../assets/profilepic.jpg";
 const About = () => {
-    return (
-        <div className="about">
-            <Image
-                src="https://static-cdn.jtvnw.net/jtv_user_pictures/a982ad29-1b19-4d8d-8a40-7285b3ed4465-profile_image-300x300.png"
-                fluid
-                roundedCircle
-                style={{ margInTop: "100px" }}
+   return (
+      <div className="about">
+         <Image
+            src={profilepic}
+            fluid
+            roundedCircle
+            style={{ margInTop: "100px", width: "50vmin" }}
+         />
+         <h1 style={{ color: 'white' }}>Ameer Mushani</h1>
+         <h2 style={{ color: 'white' }}>
+            <Typewriter
+               options={{
+                  autoStart: true,
+                  loop: true,
+               }}
+               onInit={(typewriter) => {
+                  typewriter.typeString('I am a Software Engineer')
+                     .pauseFor(300)
+                     .deleteChars(17)
+                     .typeString('Student')
+                     .pauseFor(300)
+                     .deleteChars(7)
+                     .typeString('Full Stack Developer')
+                     .pauseFor(300)
+                     .deleteChars(20)
+                     .typeString('Creator')
+                     .pauseFor(300)
+                     .deleteChars(7)
+                     .start();
+               }}
             />
-            <h1 style={{ color: 'white' }}>Ameer Mushani</h1>
-            <h2 style={{ color: 'white' }}>
-                <Typewriter
-                    options={{
-                        autoStart: true,
-                        loop: true,
-                    }}
-                    onInit={(typewriter) => {
-                        typewriter.typeString('I am a Software Engineer')
-                            .pauseFor(300)
-                            .deleteChars(17)
-                            .typeString('Student')
-                            .pauseFor(300)
-                            .deleteChars(7)
-                            .typeString('Full Stack Developer')
-                            .pauseFor(300)
-                            .deleteChars(20)
-                            .typeString('Creator')
-                            .pauseFor(300)
-                            .deleteChars(7)
-                            .start();
-                    }}
-                />
-            </h2>
-            <div className="links">
-                <a href="https://github.com/Ameer-Mushani" className="link">
-                    <GitHubIcon/>
-                </a>
-                <a href="https://www.linkedin.com/in/ameer-mushani/" className="link">
-                    <LinkedInIcon/>
-                </a>
-            </div>
-        </div>
-    )
+         </h2>
+         <div className="links">
+            <a href="https://github.com/Ameer-Mushani" className="link">
+               <GitHubIcon />
+            </a>
+            <a href="https://www.linkedin.com/in/ameer-mushani/" className="link">
+               <LinkedInIcon />
+            </a>
+         </div>
+      </div>
+   )
 }
 export default About;
